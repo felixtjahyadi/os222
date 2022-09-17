bfc7d186c5ddf2d45435436527f478fe68c84336
felix.tjahyadi@ui.ac.id <felix.tjahyadi@ui.ac.id>
2022-09-17 22:09:25 +0700

    Week02 update

--- TXT/myscript.sh
+++ TXT/myscript.sh
@@ -12,7 +12,7 @@
 # You new to set "REC2" with your own Public-Key Identity!
 # Check it out with "gpg --list-key"
 # ####################### Replace REC2 ####
-REC2="63C5CF540F195D87349A3A50D430BF3D54E906BF"
+REC2="D430BF3D54E906BF"
 # ####################### ####### #### ####
 # REC1: public key
 REC1="63FB12B215403B20"
